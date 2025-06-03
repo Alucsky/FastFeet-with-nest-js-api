944e43539fe31c492068e916cf76bd344204e1c2
Samuel Molinari <samuelmolinari8@gmail.com>
2025-06-03 17:16:07 -0300

    factory of delivery finished

--- test/factories/make-delivery.ts
+++ test/factories/make-delivery.ts
@@ -3,7 +3,10 @@ import {
   Delivery,
   DeliveryProps,
 } from "@/domain/deliveries/enterprise/entities/delivery";
+import { PrismaService } from "@/infra/database/prisma/prisma.service";
+import { PrismaDeliveryMapper } from "@/infra/database/prisma/repositories/mappers/prisma-delivery-mapper";
 import { faker } from "@faker-js/faker";
+import { Injectable } from "@nestjs/common";
 
 export function makeDelivery(
   override: Partial<DeliveryProps> = {},
@@ -20,4 +23,21 @@ export function makeDelivery(
   );
 
   return delivery;
+}
+
+@Injectable()
+export class DeliveryFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaDelivery(
+    data: Partial<DeliveryProps> = {}
+  ): Promise<Delivery> {
+    const delivery = makeDelivery(data);
+
+    await this.prisma.delivery.create({
+      data: PrismaDeliveryMapper.toPrisma(delivery),
+    });
+
+    return delivery;
+  }
 }
