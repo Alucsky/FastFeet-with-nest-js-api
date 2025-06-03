8c9888cf14d133179f41181fe4186edf73c0636c
Samuel Molinari <samuelmolinari8@gmail.com>
2025-06-03 17:15:42 -0300

    factory of deliveryman finished

--- src/infra/database/prisma/repositories/mappers/prisma-deliveryman-mapper.ts
+++ src/infra/database/prisma/repositories/mappers/prisma-deliveryman-mapper.ts
@@ -5,15 +5,24 @@ import {
   User as PrismaUser,
 } from "@prisma/client";
 
-type PrismaRecipientWithUser = PrismaDeliveryman & { user: PrismaUser };
+type PrismaDeliverymanWithUser = PrismaDeliveryman & { user: PrismaUser };
+
 export class PrismaDeliverymanMapper {
-  static toDomain(raw: PrismaRecipientWithUser): Deliveryman {
+  static toDomain(raw: PrismaDeliverymanWithUser): Deliveryman {
     return Deliveryman.create(
       {
         name: raw.user.name,
         cpf: raw.user.cpf,
+        userId: new UniqueEntityID(raw.userId),
       },
       new UniqueEntityID(raw.id)
     );
+  }
+
+  static toPrisma(deliveryman: Deliveryman): PrismaDeliveryman {
+    return {
+      id: deliveryman.id.toString(),
+      userId: deliveryman.userId.toString(),
+    };
   }
 }
--- test/factories/make-deliveryMan.ts
+++ test/factories/make-deliveryMan.ts
@@ -3,6 +3,13 @@ import { faker } from "@faker-js/faker";
 import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 
 import { DeliverymanWithPassword } from "@/domain/deliveries/enterprise/entities/value-objects/deliveryman-with-password";
+import { Injectable } from "@nestjs/common";
+import {
+  Deliveryman,
+  DeliverymanProps,
+} from "@/domain/deliveries/enterprise/entities/deliveryman";
+import { PrismaService } from "@/infra/database/prisma/prisma.service";
+import { PrismaDeliverymanMapper } from "@/infra/database/prisma/repositories/mappers/prisma-deliveryman-mapper";
 
 export function makeDeliveryman(
   override: Partial<DeliverymanWithPassword> = {},
@@ -17,4 +24,38 @@ export function makeDeliveryman(
   });
 
   return deliveryman;
+}
+
+@Injectable()
+export class DeliverymanFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaDeliveryman(): Promise<Deliveryman> {
+    const user = await this.prisma.user.create({
+      data: {
+        name: faker.person.firstName(),
+        cpf: faker.number
+          .int({ min: 10000000000, max: 99999999999 })
+          .toString(),
+        password: faker.internet.password(),
+        userType: "DELIVERYMAN",
+        role: "DELIVERYMAN",
+      },
+    });
+
+    const deliveryman = await this.prisma.deliveryman.create({
+      data: {
+        userId: user.id,
+      },
+    });
+
+    return Deliveryman.create(
+      {
+        name: user.name,
+        cpf: user.cpf,
+        userId: new UniqueEntityID(user.id),
+      },
+      new UniqueEntityID(deliveryman.id)
+    );
+  }
 }
