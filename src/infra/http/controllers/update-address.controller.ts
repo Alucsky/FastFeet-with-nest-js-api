8baf28d95d0bee4cf52d54a4ee85748723ee13f5
Samuel Molinari <samuelmolinari8@gmail.com>
2025-06-03 16:02:21 -0300

    can update an address

---
+++ src/infra/http/controllers/update-address.controller.ts
@@ -0,0 +1,56 @@
+import {
+  Controller,
+  HttpCode,
+  Body,
+  BadRequestException,
+  Put,
+} from "@nestjs/common";
+import { z } from "zod";
+import { ZodValidationPipe } from "../pipes/zod-validation-pipe";
+import { UpdateAddressUseCase } from "@/domain/recipients/application/use-cases/update-address";
+
+const updateAddressBodySchema = z.object({
+  addressId: z.string(),
+  street: z.string(),
+  number: z.string(),
+  neighborhood: z.string(),
+  city: z.string(),
+  postalCode: z.string(),
+  state: z.string(),
+});
+
+type UpdateAddressBodySchema = z.infer<typeof updateAddressBodySchema>;
+
+const validationBodyPipe = new ZodValidationPipe(updateAddressBodySchema);
+
+@Controller("/address")
+export class UpdateAddressController {
+  constructor(private updateAddressUseCase: UpdateAddressUseCase) {}
+
+  @Put()
+  @HttpCode(201)
+  async handle(@Body(validationBodyPipe) body: UpdateAddressBodySchema) {
+    const { addressId, city, neighborhood, number, postalCode, state, street } =
+      body;
+
+    const result = await this.updateAddressUseCase.execute({
+      addressId,
+      city,
+      neighborhood,
+      number,
+      postalCode,
+      state,
+      street,
+    });
+
+    if (result.isLeft()) {
+      throw new BadRequestException();
+    }
+
+    const address = result.value;
+
+    return {
+      address,
+    };
+  }
+}
