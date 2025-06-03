6f1a2581335c23316c31156133fb61ceb6b92a62
Samuel Molinari <samuelmolinari8@gmail.com>
2025-06-03 15:43:08 -0300

    can update an delivery to in progress

---
+++ src/infra/http/controllers/update-delivery-to-in-progress.controller.ts
@@ -0,0 +1,47 @@
+import {
+  Controller,
+  HttpCode,
+  Body,
+  Patch,
+  BadRequestException,
+} from "@nestjs/common";
+import { z } from "zod";
+import { ZodValidationPipe } from "../pipes/zod-validation-pipe";
+import { UpdateDeliveryToInProgressUseCase } from "@/domain/deliveries/application/use-cases/update-delivery-to-in-progress";
+
+const updateDeliveryBodySchema = z.object({
+  deliveryId: z.string(),
+  deliverymanId: z.string(),
+});
+
+type UpdateDeliveryBodySchema = z.infer<typeof updateDeliveryBodySchema>;
+
+const validationBodyPipe = new ZodValidationPipe(updateDeliveryBodySchema);
+
+@Controller("/delivery")
+export class UpdateDeliveryToInProgressUseCaseController {
+  constructor(
+    private updateDeliveryUseCase: UpdateDeliveryToInProgressUseCase
+  ) {}
+
+  @Patch()
+  @HttpCode(201)
+  async handle(@Body(validationBodyPipe) body: UpdateDeliveryBodySchema) {
+    const { deliveryId, deliverymanId } = body;
+
+    const result = await this.updateDeliveryUseCase.execute({
+      deliveryId,
+      deliverymanId,
+    });
+
+    if (result.isLeft()) {
+      throw new BadRequestException();
+    }
+
+    const delivery = result.value;
+
+    return {
+      delivery,
+    };
+  }
+}
