fed4e650a6ca614cfb2a1a7149b58a8526a7ba94
Samuel Molinari <samuelmolinari8@gmail.com>
2025-06-03 15:31:27 -0300

    can update delivery to delivered

---
+++ src/infra/http/controllers/update-delivery-to-delivered.controller.ts
@@ -0,0 +1,43 @@
+import { Controller, HttpCode, Body, Patch, BadRequestException } from "@nestjs/common";
+import { z } from "zod";
+import { ZodValidationPipe } from "../pipes/zod-validation-pipe";
+import { UpdateDeliveryToDeliveredUseCase } from "@/domain/deliveries/application/use-cases/update-delivery-to-delivered";
+
+const updateDeliveryBodySchema = z.object({
+  deliveryId: z.string(),
+  deliverymanId: z.string(),
+  deliveryConfirmationUrl: z.string(),
+});
+
+type UpdateDeliveryBodySchema = z.infer<typeof updateDeliveryBodySchema>;
+
+const validationBodyPipe = new ZodValidationPipe(updateDeliveryBodySchema);
+
+@Controller("/delivery")
+export class UpdateDeliveryToDeliveredController {
+  constructor(
+    private updateDeliveryUseCase: UpdateDeliveryToDeliveredUseCase
+  ) {}
+
+  @Patch()
+  @HttpCode(201)
+  async handle(@Body(validationBodyPipe) body: UpdateDeliveryBodySchema) {
+    const { deliveryId, deliverymanId, deliveryConfirmationUrl } = body;
+
+    const result = await this.updateDeliveryUseCase.execute({
+      deliveryConfirmationUrl,
+      deliveryId,
+      deliverymanId,
+    });
+
+    if (result.isLeft()) {
+      throw new BadRequestException();
+    }
+
+    const delivery = result.value;
+
+    return {
+      delivery,
+    };
+  }
+}
