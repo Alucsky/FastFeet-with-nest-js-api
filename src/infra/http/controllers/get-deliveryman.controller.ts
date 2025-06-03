f0d5993cc4b0190b44633049e685545ff6389d89
Samuel Molinari <samuelmolinari8@gmail.com>
2025-06-03 15:18:56 -0300

    get deliveryman

---
+++ src/infra/http/controllers/get-deliveryman.controller.ts
@@ -0,0 +1,33 @@
+import { Controller, HttpCode, Body, Get } from "@nestjs/common";
+import { z } from "zod";
+import { ZodValidationPipe } from "../pipes/zod-validation-pipe";
+import { GetDeliverymanUseCase } from "@/domain/deliveries/application/use-cases/get-deliveryman";
+
+const getDeliverymanBodySchema = z.object({
+  deliverymanId: z.string(),
+});
+
+type GetDeliverymanBodySchema = z.infer<typeof getDeliverymanBodySchema>;
+
+const validationBodyPipe = new ZodValidationPipe(getDeliverymanBodySchema);
+
+@Controller("/deliveryman")
+export class GetDeliverymanController {
+  constructor(private getDeliverymanUseCase: GetDeliverymanUseCase) {}
+
+  @Get()
+  @HttpCode(201)
+  async handle(@Body(validationBodyPipe) body: GetDeliverymanBodySchema) {
+    const { deliverymanId } = body;
+
+    const result = await this.getDeliverymanUseCase.execute({
+      deliverymanId,
+    });
+
+    const deliveryman = result.value;
+
+    return {
+      deliveryman,
+    };
+  }
+}
